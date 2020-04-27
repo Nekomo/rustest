a264b6beebf0f3fcc02f5f651f6ba0762e56fe81
Mucha <daisanbukan@gmail.com>
2020-04-27 22:34:45 +0900

    挿入ソートを取りあえず動く版に変更

--- AOJ/sorts/src/main.rs
+++ AOJ/sorts/src/main.rs
@@ -1,23 +1,8 @@
 use proconio::input;
-use std::any::type_name;
 
-fn type_of<T>(_: T) -> &'static str {
-    type_name::<T>()
-}
-fn array_to_string(array:Vec<i32>)
-{
-  let st = String::new();
-  for a in array{
-    st+=a;
-    st+=' ';
-  }
-  st
-}
 fn insertion_sort(array:&mut Vec<i32>){
-    //for (i,a) in array.iter_mut().enumerate(){
     println!("{:?}",array);
     for i in 1 .. array.len(){
-      //println!("val=({},{}),type=({},{})",i,a,type_of(i),type_of(a))
       //①位置決定
       let mut insertpos:usize=i;
       for j in (0..i).rev(){
@@ -29,15 +14,12 @@ fn insertion_sort(array:&mut Vec<i32>){
             insertpos=0;
           }
       }
-      //println!("{:?}",array);
-      //println!("{},{}",i,insertpos);
       //②挿入
       for j in (insertpos..i).rev()
       {
         array.swap(j,j+1);
       }
-      let arrstr=array_to_string(&array);
-      println!("{}",arrstr);
+      println!("{:?}",array);
     }
     println!("{:?}",array);
 }
