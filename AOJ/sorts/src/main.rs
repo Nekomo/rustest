482db29935699fb4ef0e930b8e42fb35ae00c194
Mucha <daisanbukan@gmail.com>
2020-04-30 00:36:24 +0900

    選択ソートを実装、ALDS 1_2_BでAC

--- AOJ/sorts/src/main.rs
+++ AOJ/sorts/src/main.rs
@@ -28,23 +28,44 @@ fn insertion_sort(array:&mut Vec<i32>){
   //print_array(array.to_vec());
 }
 
-fn bubble_sort(array:&mut Vec<i32>)
+fn bubble_sort(array:&mut Vec<i32>)->i32
 {
-  for i in (0..array.len()).rev(){
-    let mut current_pos=i;
-    for j in (1..array.len()).rev(){
-      if array[j]<array[j-1]
-      {
-        array.swap(j,j-1);
-        print_array(array.to_vec());
-      }
-      else {
-        break;
-      }
-    } 
+  let mut swap_times=0;
+  let mut swapped=true;
+  while swapped{
+  swapped=false;
+  for j in (1..array.len()).rev(){
+    if array[j]<array[j-1]
+    {
+      array.swap(j,j-1);
+      //print_array(array.to_vec());
+      swap_times+=1;
+      swapped=true;
+    }
   }
 }
-
+  swap_times
+}
+fn selection_sort(array:&mut Vec<i32>)->i32{
+  let mut swap_cnt=0;
+  for i in 0..array.len(){
+    let mut current_min=1145141919;
+    let mut current_min_pos=array.len()-1;
+    for j in i..array.len()
+    {
+      if current_min>array[j]{
+        current_min_pos=j;
+        current_min=array[j]
+      }
+    }
+    if current_min_pos!=i {
+      array.swap(i,current_min_pos);
+      swap_cnt+=1;
+    }
+    //print_array(array.to_vec());
+  }
+  swap_cnt
+}
 fn main() {
   //入力を受け取ってi32に変換
   let n:i32 = readline().parse().unwrap();
@@ -52,8 +73,10 @@ fn main() {
   //let mut array=vec![5,4,3,2,1];
   let mut array=readline_as_vec();
   //insertion_sort(&mut array);
-  bubble_sort(&mut array);
-
+  //let swap_occured=bubble_sort(&mut array);
+  let swap_occured=selection_sort(&mut array);
+  print_array(array);
+  println!("{:?}",swap_occured);
 }
 
 fn print_array(array:Vec<i32>){
